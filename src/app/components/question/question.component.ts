32ffd8dfae7bd13ac65f92f93ef398fa929cf930
szalo13 <kamil_sza@yahoo.pl>
2017-02-24 06:27:55 +0100

    modularizing comments

--- src/app/components/question/question.component.ts
+++ src/app/components/question/question.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-question',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./question.component.scss']
 })
 export class QuestionComponent implements OnInit {
+
+  @Input() question;
 
   constructor() { }
 
--- src/app/pages/single-question/single-question.component.ts
+++ src/app/pages/single-question/single-question.component.ts
@@ -1,13 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { QuestionsService } from '../../services/questions.service';
 
 @Component({
   selector: 'app-single-question',
   templateUrl: './single-question.component.html',
-  styleUrls: ['./single-question.component.scss']
+  styleUrls: ['./single-question.component.scss'],
+  providers: [ QuestionsService ]
 })
 export class SingleQuestionComponent implements OnInit {
 
-  constructor() { }
+  private questions;
+  private question;
+
+  constructor(
+    questionsService: QuestionsService
+  ) {
+    this.questions = questionsService.getQuestions();
+    this.question = (this.questions[1]);
+  }
+
 
   ngOnInit() {
   }
